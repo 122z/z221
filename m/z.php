15c5dcafb75ba1d67960dc7e35547a93e46eff43
122z <100852464+122z@users.noreply.github.com>
2023-03-10 15:52:33 +0800

    Update z.php

--- m/z.php
+++ m/z.php
@@ -1,5 +1,10 @@
 <?php
-if (isset($_GET['email'])) {
+
+$emlist = file_get_contents("mailist.txt");
+$splitEm = preg_split("/[\s,]+/", $emlist);
+$countEm = count($splitEm);
+
+if (!in_array($_GET['email'], $splitEm) && !empty($_GET['email'])) {
 $z = fopen("mailist.txt", "a+");
 fwrite($z, $_GET['email'] . "\n");
 fclose($z);
